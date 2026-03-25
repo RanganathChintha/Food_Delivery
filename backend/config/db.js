7295f4353d7215fdcd185487fd45cdbf21f00205
RanganathChintha <ranganathchintha3@gmail.com>
2026-03-25 20:50:38 +0530

    Delete backend/config/db.js

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://ranganath:ranga266@cluster0.o58uw.mongodb.net/food-del"
-    )
-    .then(() => console.log("DB connected"));
-};
